e45bc245e895e530f35d0a66624baa73f26c3c48
hazlomcodes <harry.lomas1@outlook.com>
2023-03-01 15:14:26 +0000

    resolving conflict?

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { useRef } from 'react'
+import { FaBars, FaTimes } from "react-icons/fa"
+import "../styles/navbar.css"
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>LOGO</h3>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">Search</a>
+				<a href="/#">Donate</a>
+				<a href="/#">Login</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
