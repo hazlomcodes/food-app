3e344c77648c3b485464add114ae1e40535d99d4
hazlomcodes <harry.lomas1@outlook.com>
2023-03-09 11:51:08 +0000

    facebook login working

--- src/components/login.js
+++ src/components/login.js
@@ -1,87 +1,56 @@
-import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-import "../styles/login.css";
+import React, {Component} from 'react';
+import FacebookLogin from 'react-facebook-login';
 
-const Login = () => {
-  const { setAuth } = useContext(AuthContext);
-  const userRef = useRef();
-  const errRef = useRef();
+export default class Facebook extends Component {
 
-  const [user, setUser] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [success, setsuccess] = useState(false);
+    state = {
+        auth: false,
+        name: '',
+        picture: ''
+    };
 
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
+    responseFacebook = response => {
+        console.log(response);
+        if(response.status !== 'unknown')
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url
+        });
 
-  useEffect(() => {
-    setErrMsg("");
-  }, [user, pwd]);
+    }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(user, pwd);
-    setUser("");
-    setPwd("");
-    setsuccess(true);
-  };
+    componentClicked = () => {
+        console.log('Facebook btn clicked');
+    }
 
-  return (
-    <>
-      {success ? (
-        <section>
-          <h1> You are logged in!</h1>
-          <br />
-          <p>
-            <a href="#"> Go to Home </a>
-          </p>
-        </section>
-      ) : (
-        <section>
-          <p
-            ref={errRef}
-            className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive"
-          >
-            {errMsg}
-          </p>
-          <h1> Sign In</h1>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setUser(e.target.value)}
-              value={user}
-              required
-            />
+    render(){
+        let fbContent;
 
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
-              required
-            />
-            <button>Sign In</button>
-          </form>
-          <p>
-            {" "}
-            Need an Account
-            <br />
-            <span className="line">
-              <a href="#">Sign Up</a>
-            </span>
-          </p>
-        </section>
-      )}
-    </>
-  );
-};
+        this.state.auth ?
+            fbContent = (
+                <div style={{
+                    width: '400px',
+                    margin: 'auto',
+                    background: '#f4f4f4',
+                    padding: '20px',
+                    color: '#000'
+                }}>
+                    <img src={this.state.picture} alt={this.state.name} />
+                    <h2>Welcome {this.state.name}!</h2>
+                </div>
+            ) : 
+            fbContent = (<FacebookLogin
+                appId="1542594726233782"
+                autoLoad={true}
+                fields="name,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />);
 
-export default Login;
+        return (
+            <div>
+                {fbContent}
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import "../src/styles/index.css";
+import "./styles/index.css";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
