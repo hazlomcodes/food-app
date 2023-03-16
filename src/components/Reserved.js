8607f00681405990d434129cbb2bd05b04a0733e
hazlomcodes <harry.lomas1@outlook.com>
2023-03-16 12:56:12 +0000

    created Reserved items component and hardcoded data

--- src/components/Reserved.js
+++ src/components/Reserved.js
@@ -4,13 +4,15 @@ import '../styles/reserved.css'
 
 const Reserved = () => {
   const [donations, setDonations] = useState([
-    { name:'apple', quantity:'1', expiration:'01/01/0101', id: 1 },
-    { name:'banna', quantity:'2', expiration:'02/02/0202', id: 2 },
-    { name:'orange', quantity:'3', expiration:'03/03/03', id: 3 },
+    { name:'apple', quantity:' 1', expiration:' 01/01/0101', id: 1 },
+    { name:'banna', quantity:' 2', expiration:' 02/02/0202', id: 2 },
+    { name:'orange', quantity:' 3', expiration:' 03/03/03', id: 3 },
   ]);
 
   return( 
   <div className="reserved">
+
+    <h1 className='user-reserved'> Your Reserved Items</h1>
 
     {donations.map((donation) => (
       <div className='donation-preview' key={donation.id}>
